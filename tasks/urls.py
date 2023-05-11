e268acc7582397a3e962d0555b6e8442cb09e327
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-05-11 08:37:04 -0400

    create signout

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    path('tasks/', views.tasks, name='tasks')
+    path('tasks/', views.tasks, name='tasks'),
+    path('logout/', views.signout, name='logout')
+
 ]
