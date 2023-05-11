cb02e30c4df3d76622ab5d915d39031641fbfe03
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-05-11 13:43:49 -0400

    crear tareas

--- tasks/views.py
+++ tasks/views.py
@@ -66,7 +66,14 @@ def create_task(request):
             'form':TaskForm
         })
     else:
-        print(request.POST)
-        return render(request, 'create_task.html', {
-            'form':TaskForm
-        })
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {
+                'form':TaskForm,
+                'error': 'please provide valida data'
+            })
