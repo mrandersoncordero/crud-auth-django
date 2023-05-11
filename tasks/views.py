8ac24e13afef0ad4fd9d2c01d32fc80483a72b45
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-05-11 08:13:35 -0400

    redirigir usuario a tasks y manejar errores con except

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 from django.http import HttpResponse
+from django.db import IntegrityError
 
 # Create your views here.
 def home(request):
@@ -18,8 +20,9 @@ def signup(request):
             try:
                 user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('User created successfully')
-            except:
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
                 return render(request, 'signup.html', {
                     'form': UserCreationForm,
                     'error': 'Username already exists'
@@ -29,4 +32,5 @@ def signup(request):
             'error': 'Password do not match'
         })
 
-
+def tasks(request):
+    return render(request, 'tasks.html')
