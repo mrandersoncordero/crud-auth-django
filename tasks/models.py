5d00e89c264f80d6d8588302ad6d055351abbc01
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-05-11 13:26:58 -0400

    cambiar nombre de colmna description

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    desciption = models.TextField(blank=True)
+    description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     datecompleted = models.DateTimeField(null=True)
     important = models.BooleanField(default=False)
